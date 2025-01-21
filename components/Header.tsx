6f98e0c47a9994da4d80fb0595d0ec46808f209d
Paras Dhananjay Kuranjekar <paraskuranjekar@gmail.com>
2025-01-21 18:00:53 +0530

    commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,13 +7,14 @@ import { usePathname } from "next/navigation"
 import { Menu } from "lucide-react";
 import Sidebar from "./Sidebar";
 import { useState } from "react";
+import {CiMenuFries} from "react-icons/ci";
 
 const Header = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const pathname = usePathname();
 
   return (
-    <header className="border-b border-b-hoverColor/50 bg-bodyColor text-white sticky top-0 z-50">
+    <header className="bg-bodyColor text-white sticky top-0 z-50">
         <Container className="py-5 flex items-center justify-between">
             <Logo title="Paras" subtitle="."/>
             <div className="hidden md:inline-flex items-center gap-7 text-sm uppercase tracking-wide font-medium">
@@ -26,7 +27,7 @@ const Header = () => {
               ))}
               <Link href={'/resume.pdf'} target="_blank" rel="noopener noreferer" className="text-sm bg-lightSky/10 px-4 py-2 rounded-md border border-hoverColor/10 hover:border-hoverColor hover:bg-hoverColor hover:text-black hoverEffect">Hire me</Link>
             </div>
-            <button aria-label="Toggle menu" onClick={()=>setIsSidebarOpen(!isSidebarOpen)} className="inline-flex md:hidden relative hover:text-hoverColor hoverEffect"><Menu/></button>
+            <button aria-label="Toggle menu" onClick={()=>setIsSidebarOpen(!isSidebarOpen)} className="inline-flex md:hidden relative hover:text-hoverColor hoverEffect"><CiMenuFries/></button>
         </Container>
         <div className="md:hidden">
           <Sidebar isOpen={isSidebarOpen} onClose={()=>setIsSidebarOpen(false)} pathname={pathname}/>
